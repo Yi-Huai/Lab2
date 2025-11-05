8f8dcd9fb3a8548922a5835573a9d50221c087ef
YiHuai <YIHUAI.23@ichat.sp.edu.sg>
2025-11-05 10:27:54 +0800

    3

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,28 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+
+def get_user_input():
+    # 1. Read numbers entered by the user
+    user_input = input()
+
+    # 2. Split the string into a list of strings using ',' as separator
+    str_list = user_input.split(',')
+
+    # 3. Convert the list of strings to a list of floats
+    
+
+    # 4. Return the list of floats
+    
+
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    print(num_list)
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ bmi.py
@@ -0,0 +1,19 @@
+def calculate_bmi(height, weight):
+
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi= weight/(height*height)
+    
+    if(bmi<18.5):
+        print("Under Weight")
+        return -1
+    elif(18.5 <= bmi <= 25.0):
+        print("Normal Weight")
+        return 0
+    elif(bmi > 25.0):
+        print("Over Weight")
+        return 1
+
+calculate_bmi(weight=57, height=1.73)
+
