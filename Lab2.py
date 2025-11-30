4a55dff8a715ffd241f37964f5818a29df74b2e6
YiHuai <YIHUAI.23@ichat.sp.edu.sg>
2025-12-01 03:10:43 +0800

    redo

--- Lab2.py
+++ Lab2.py
@@ -16,7 +16,21 @@ def get_user_input():
     return float_list
     # 4. Return the list of floats
     
+def calc_average_temperature(float_list):
+    print("calc_average_temperature")
+    avg = sum(float_list) / len(float_list)
+    return float(avg)
 
+def calc_min_max_temperature(float_list):
+    max_temp = max(float_list)
+    min_temp = min(float_list)
+    return [min_temp, max_temp]
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("calc_median_temperature")
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab2 - Introduction to Python")
